83b45b4f2a5cab8fdd5b3de16f2d270b1d8663dc
thoamsy <c6swift@gmail.com>
2020-08-02 18:07:33 +0800

    Initial Commit

---
+++ Shared/SwiftUI_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GalleryApp.swift
+//  Shared
+//
+//  Created by yk on 2020/8/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
