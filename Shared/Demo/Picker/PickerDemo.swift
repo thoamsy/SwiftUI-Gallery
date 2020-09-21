dc34c3320542d2ff9f3d77e595cd82a2eb022fd4
thoamsy <c6swift@gmail.com>
2020-09-22 00:13:21 +0800

    加一个 form 模式的 picker

--- Shared/Demo/Picker/PickerDemo.swift
+++ Shared/Demo/Picker/PickerDemo.swift
@@ -15,12 +15,13 @@ enum PickerStyle: String, CaseIterable, Identifiable {
   case `default`
   case wheel
   case segmented
+  case form
 }
 
 extension View {
   @ViewBuilder func pickerStyle(_ style: PickerStyle) -> some View {
     switch style {
-      case .default:
+      case .default, .form:
         self.pickerStyle(DefaultPickerStyle())
       case .wheel:
         self.pickerStyle(WheelPickerStyle())
@@ -35,36 +36,46 @@ enum Flavor: String, CaseIterable, Identifiable {
   case vanilla
   case strawberry
 
+
   var id: String { self.rawValue }
 }
 
 struct PickerDemo: View {
   @State private var currentStyle = PickerStyle.default
   @State private var selectedFlavor = Flavor.chocolate
-  
+
+  var picker: some View {
+    Picker("Flavor", selection: $selectedFlavor) {
+      Text("Chocolate").tag(Flavor.chocolate)
+      Text("Vanilla").tag(Flavor.vanilla)
+      Text("Strawberry").tag(Flavor.strawberry)
+    }.pickerStyle(currentStyle)
+  }
 
   var body: some View {
-    VStack {
-      Picker("", selection: $currentStyle) {
-        ForEach(PickerStyle.allCases) {
-          Text($0.rawValue.capitalized).tag($0)
-        }
-      }.pickerStyle(SegmentedPickerStyle())
+    NavigationWrapper(title: "Yes") {
+      VStack {
+        Picker("", selection: $currentStyle) {
+          ForEach(PickerStyle.allCases) {
+            Text($0.rawValue.capitalized).tag($0)
+          }
+        }.pickerStyle(SegmentedPickerStyle())
 
-      Picker("Flavor", selection: $selectedFlavor) {
-        Text("Chocolate").tag(Flavor.chocolate)
-        Text("Vanilla").tag(Flavor.vanilla)
-        Text("Strawberry").tag(Flavor.strawberry)
-      }.pickerStyle(currentStyle)
-      Text("Selected flavor: \(selectedFlavor.rawValue)")
+        if currentStyle == .form {
+          Form {
+            picker
+          }
+        } else {
+          picker
+        }
+        Text("Selected flavor: \(selectedFlavor.rawValue)")
+      }
     }
   }
 }
 
 struct PickerDemo_Previews: PreviewProvider {
   static var previews: some View {
-    NavigationWrapper(title: "Yes") {
-      PickerDemo()
-    }
+    PickerDemo()
   }
 }
